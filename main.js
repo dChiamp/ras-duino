61088af078d1cd3c53d21ca547d1679a10818089
dChiamp <derek.chiampas@gmail.com>
2016-06-18 18:07:19 +0000

    adds working tether, servo, led

---
+++ main.js
@@ -0,0 +1,29 @@
+var five = require("johnny-five");
+var board = new five.Board();
+var Raspi = require("raspi-io");
+
+board.on("ready", function() {
+  var led = new five.Led(12);
+
+  
+  var servo = new five.Servo({
+   pin: 10, 
+   range: [ 0, 180 ],
+   startAt: 120
+  });
+  
+  this.repl.inject({
+    led: led, 
+    servo: servo
+  });
+
+
+  //servo.sweep();
+  led.blink();
+  
+  
+
+
+
+});
+
