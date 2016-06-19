ab52872bfe6973595f8dc5b92dd552d988f64617
dchiamp <derek.chiampas@gmail.com>
2016-06-19 12:59:40 -0700

    reconfigs server

---
+++ server.js
@@ -0,0 +1,67 @@
+var express = require('express');
+var logger = require('morgan');
+var path = require('path');
+var bodyParser = require('body-parser');
+var fs = require('fs');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+var raspi = require("raspi-io");
+var five = require("johnny-five"),
+  board,servo;
+
+// app setup
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// views setup
+app.use("/", express.static(__dirname + "/public"));
+
+// catch all routes
+app.get('/*', function(req, res){
+  console.log("HIT!")
+  res.sendFile(process.cwd() + '/public/views/index.html')
+})
+
+
+app.listen(3000, function(){
+    console.log('Server running on port 3000');
+});
+
+board = new five.Board();
+
+board.on("ready", function() {
+
+  servo = new five.Servo(10);
+
+  board.repl.inject({
+    servo: servo
+  });
+
+  
+  servo.sweep();
+
+  this.wait(5000, function(){
+    servo.stop();
+    servo.to(oldAng);
+  servo.stop();
+  });
+
+  
+  io.on('connection', function (socket) {
+  
+    socket.on("changeAngle",function(ang, control){
+    deltAng = oldAng - ang;
+    oldAng = ang;
+    servo.step(deltAng);
+        console.log(oldAng, control);
+    });
+
+  });
+  
+
+});
+
+
+module.exports = app;
